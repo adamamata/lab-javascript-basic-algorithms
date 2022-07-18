36244aa82c7cd8c13839c06cc465d9c59b57363b
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-18 16:32:36 +0200

    changed bonus 1

--- js/index.js
+++ js/index.js
@@ -51,9 +51,16 @@ else {
 //Bonus 1
 let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget fermentum nibh. Proin tristique felis consectetur magna egestas suscipit. Etiam euismod, turpis ac vehicula pulvinar, est est iaculis ipsum, molestie aliquet sem libero sit amet eros. Sed laoreet tortor sapien. Aliquam gravida pharetra leo, a ultricies neque ullamcorper at. Aliquam nec sapien quis metus pretium commodo. Sed ac lacus non sapien lacinia vulputate. Maecenas a iaculis massa. Sed tempor, lectus id congue sollicitudin, urna justo pulvinar diam, nec viverra risus massa at ex. Nullam metus elit, vestibulum dictum odio sed, suscipit dignissim nisl. Mauris sed mi ligula. Vestibulum malesuada, arcu vitae tincidunt consectetur, elit odio bibendum augue, eget lobortis dui augue ac urna. Cras facilisis urna ut turpis luctus rutrum. Praesent efficitur justo massa, ac molestie felis volutpat eu. Donec eget congue augue, ac bibendum libero. Donec finibus vestibulum sem, a ultricies nulla faucibus id. Nullam mattis massa non pharetra viverra. Pellentesque et lacus porta, efficitur ligula at, condimentum lectus. Vestibulum eget lorem dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce vehicula enim nisi, id ullamcorper metus bibendum id. Nunc leo neque, tincidunt et nisi non, vehicula sodales neque. Vestibulum fermentum dolor ut metus auctor, eu pretium urna convallis. Fusce id efficitur nisi. Cras facilisis nisi sit amet libero iaculis ornare. Vivamus malesuada sem eu elit elementum, in convallis neque dapibus. Nam tempus aliquet ligula, non rhoncus leo dignissim ac. Quisque posuere vulputate sollicitudin. Duis non lacus vitae sem maximus hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id imperdiet neque, quis auctor mi. Praesent sit amet laoreet ipsum. Nam non elementum eros. Sed a orci vitae massa iaculis posuere vitae vitae justo.";
 let count = 1;
-for (let i = 0; i <= text.length; i++){
+for (let i = 0; i < text.length; i++){
     if (text[i] === " "){
         count += 1;
+    } else if (text ===""){
+        count = 0; 
     }
 }
-console.log(count);
+console.log(count); 
+
+let etCount = 0; 
+for (let i = 0; i <= text.length; i++){
+    
+}
